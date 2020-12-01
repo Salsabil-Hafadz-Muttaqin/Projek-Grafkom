3f0673a8a5c0d78ee592f7c92f3708aed64fbafd
Salsabil-Hafadz-Muttaqin <salsabilhafadzmuttaqin@gmail.com>
2020-12-01 19:37:42 +0700

    timer, input sama background belum logika

--- Nyobain Timer/main.cpp
+++ Nyobain Timer/main.cpp
@@ -26,21 +26,12 @@ void ColliderPersegi(){ //Collider berbentuk kotak
 void ObjekUlar(){
     glRotatef(angle,0,0,1);
     glBegin(GL_POLYGON);
-    glColor3f(0,0,0);
-    glVertex2f(0.1,1);
-    glVertex2f(1.5,1);
-    glVertex2f(1.2,2);
-    glVertex2f(1.46,3);
-    glVertex2f(1.37,4);
-    glVertex2f(1.3,4.3);
-    glVertex2f(1.39,4.6);
-    glVertex2f(1.09,4.99);
-    glVertex2f(1,4.99);
-    glVertex2f(0.7,4.61);
-    glVertex2f(0.84,4.31);
-    glVertex2f(0.78,4);
-    glVertex2f(0.8,3);
-    glVertex2f(1,1);
+    glColor3f(1.0,1.0,0.5);
+    glVertex2f(80, 5);
+    glVertex2f(81, 19);
+    glVertex2f(80.5, 20);
+    glVertex2f(79.5, 20);
+    glVertex2f(79, 19);
     glEnd();
 }
 
@@ -50,7 +41,7 @@ void UlarObjek(){
 
     glPushMatrix();
     glTranslatef(horizontalMove, verticalMove,0); //mengendalikan posisi ular
-    glTranslatef(20,20,0); //menengahkan view
+    glTranslatef(-28,0,0); //menengahkan view
     ObjekUlar(); //memanggil view ular
     glPopMatrix();
 }
@@ -68,7 +59,7 @@ void charController(int data)
     }
     //jika menekan tombol panah kanan
     else if (GetAsyncKeyState(VK_RIGHT)){
-        if (posisiX[5] <= 50){
+        if (posisiX[5] <= 57){
             posisiX[0] += 0.1f;
             posisiX[5] += 0.1f;
             horizontalMove += 0.1f;
@@ -76,7 +67,7 @@ void charController(int data)
     }
     //jika menekan tombol panah atas
     if (GetAsyncKeyState(VK_UP)){
-        if (posisiY[5] <= 50){
+        if (posisiY[5] <= 70){
             posisiY[0] += 0.1f;
             posisiY[5] += 0.1f;
             verticalMove += 0.1f;
@@ -105,8 +96,85 @@ void keyboard(unsigned char key, int x, int y)
 }
 
 void displayMe(void){
-    glClearColor(1,1,1,1);
+
     glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glColor3b(247, 105, 105);
+    glVertex2f(0, 0);
+    glVertex2f(0, 90);
+    glVertex2f(160, 90);
+    glVertex2f(160, 0);
+    glEnd();
+
+
+    glBegin(GL_POLYGON);
+    glColor3b(0, 0, 0);
+    glVertex2f(50, 0);
+    glVertex2f(50, 90);
+    glVertex2f(110, 90);
+    glVertex2f(110, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0,0.0,1.0);
+    glVertex2f(50, 0);
+    glVertex2f(50, 90);
+    glVertex2f(51, 90);
+    glVertex2f(51, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0,0.0,1.0);
+    glVertex2f(70, 0);
+    glVertex2f(70, 90);
+    glVertex2f(71, 90);
+    glVertex2f(71, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0,0.0,1.0);
+    glVertex2f(90, 0);
+    glVertex2f(90, 90);
+    glVertex2f(91, 90);
+    glVertex2f(91, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(0.0,0.0,1.0);
+    glVertex2f(110, 0);
+    glVertex2f(110, 90);
+    glVertex2f(111, 90);
+    glVertex2f(111, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(1.0,1.0,1.0);
+    glVertex2f(54, 30);
+    glVertex2f(53, 35);
+    glVertex2f(62, 38);
+    glVertex2f(68, 35);
+    glVertex2f(68, 30);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3f(1.0,1.0,0.0);
+    glVertex2f(93, 56);
+    glVertex2f(98, 58);
+    glVertex2f(106, 60);
+    glVertex2f(108, 56);
+    glVertex2f(104, 55);
+    glVertex2f(106, 53);
+    glVertex2f(98, 51);
+    glVertex2f(95, 53);
+    glEnd();
+
+
+
+
+	glutSwapBuffers();
+
+    glClearColor(1,1,1,1);
+    //glClear(GL_COLOR_BUFFER_BIT);
     //memanggil ular
     UlarObjek();
 
@@ -128,10 +196,10 @@ int main (int argc, char**argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutInitWindowSize(1000,1000);
+    glutInitWindowSize(1000, 1000);
     glutInitWindowPosition(0,0);
     glutCreateWindow("Nyobain Timer");
-    gluOrtho2D(0,50,0,50);
+    gluOrtho2D(0, 160, 0, 90);
     glutDisplayFunc(displayMe);
     glutTimerFunc(1,charController,0);
     glutKeyboardFunc(keyboard);
