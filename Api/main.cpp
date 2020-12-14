6458e943747acf6b37a0ad102c424ef592ae9414
smtmtc <smtmtc@gmail.com>
2020-12-14 10:49:07 +0700

    Update

---
+++ Api/main.cpp
@@ -0,0 +1,336 @@
+#include <windows.h>
+#include <GL/gl.h>
+#include <Gl/glu.h>
+#include <GL/glut.h>
+
+void Api(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.4, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glTranslatef(40.0, 0.0, 0.0);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.4, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glTranslatef(40.0, 0.0, 0.0);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.4, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glTranslatef(40.0, 0.0, 0.0);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.4, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glTranslatef(0.0, -2.0, 0.0);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.7, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glTranslatef(-40.0, 0.0, 0.0);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.7, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glTranslatef(-40.0, 0.0, 0.0);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.7, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glTranslatef(-40.0, 0.0, 0.0);
+    glBegin(GL_POLYGON); //Api
+    glColor3f(1.0, 0.7, 0.0);
+    glVertex2f(0,0); //M
+    glVertex2f(41.97, 0.00); //N
+    glVertex2f(41.97, 3.38); //A
+    glVertex2f(40.93, 5.56); //B
+    glVertex2f(37.79, 8.31); //B
+    glVertex2f(38.27, 6.13); //B
+    glVertex2f(37.89, 4.14); //B
+    glVertex2f(35.99, 3.00); //B
+    glVertex2f(33.81, 4.99); //B
+    glVertex2f(33.62, 7.37); //B
+    glVertex2f(31.24, 4.61); //B
+    glVertex2f(30.29, 3.00); //B
+    glVertex2f(28.97, 7.08); //B
+    glVertex2f(26.97, 7.08); //B
+    glVertex2f(26.59, 9.36); //B
+    glVertex2f(24.69, 6.70); //B
+    glVertex2f(23.56, 8.98); //B
+    glVertex2f(21.66, 4.9); //B
+    glVertex2f(18.90, 2.62); //B
+    glVertex2f(16.44, 3.28); //B
+    glVertex2f(15.01, 3.95); //B
+    glVertex2f(14.92, 6.42); //B
+    glVertex2f(13.30, 4.04); //B
+    glVertex2f(12.16, 2.33); //B
+    glVertex2f(11.03, 5.94); //B
+    glVertex2f(9.13, 7.65); //B
+    glVertex2f(9.12, 5.75); //B
+    glVertex2f(7.80, 3.57); //B
+    glVertex2f(5.99, 4.42); //B
+    glVertex2f(5.43, 6.89); //B
+    glVertex2f(6.09, 10.02); //B
+    glVertex2f(2.58, 6.89); //B
+    glVertex2f(0.87, 4.71); //B
+    glVertex2f(0, 2.14); //B
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void init() { // background gambar
+    glClearColor(1.0,1.0,1.0,1.0);//warna backgroung
+}
+int main(int argc, char** argv){ // untuk jendelanya
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);//membuat jendela yg double buffered
+	glutInitWindowSize(728, 728);//size jendela
+	glutInitWindowPosition(0,0); //pojok bawah kiri
+	glutCreateWindow("API"); // nama jendela
+	glutDisplayFunc(Api);//memanggil fungsi display Me
+	init();
+	gluOrtho2D(0, 160, 0, 90); // inisialisasi berapa kotak yg mau dipakai
+	glutMainLoop();//untuk menjalan kan program, semua fungsi dieksekusi
+	return 0;
+}
